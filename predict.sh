8623580666097880115b413ecf027afa6c4bfdb5
Jordan J. Henkel <jjhenkel@cs.wisc.edu>
2018-10-04 00:42:20 -0500

    Updated to use model trained on all data.

--- predict.sh
+++ predict.sh
@@ -3,7 +3,7 @@
 echo "[instauro] Running featurizer (this can be SLOW)..."
 python3 /app/src/featurizer.py /data/Tasks > /tmp/features.txt
 echo "[instauro] Ranking use pre-trained model..."
-java -jar /app/lib/RankLib-2.8.jar -load /app/models/pDS-2.model -rank /tmp/features.txt -score /tmp/scores.txt
+java -jar /app/lib/RankLib-2.8.jar -load /app/models/pDS-all.model -rank /tmp/features.txt -score /tmp/scores.txt
 echo "[instauro] Making predictions (this can also take a while)..."
 python3 /app/src/guesser.py /tmp/scores.txt /data/Tasks | python3 /app/src/evaluate.py -d /data
 echo "[instauro] Done!"
